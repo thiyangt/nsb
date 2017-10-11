367eba1779077faf225af64c74eb77af78e0d659
thiyangt <thiyanga.talagala@monash.edu>
2017-10-12 08:48:51 +1100

    update the readme file

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#'@ Best forecasting method based on MASE for 1290000 series
+#'@description Best forecasting method (based on MASE) for 1290000 simulated time series
+#'@format simA: list of 645 elements, each element is a matrix of 1000 columns
+#'@format simE: list of 645 elements, each element is a matrix of 1000 columns
+#'@format BESTsimA: list of 645 elements, each list is a vector contains the best forecasting methods of simA
+#'@format BESTsimE: list of 645 elements, each list is a vector contains the best forecasting methods of simA
